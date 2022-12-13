edabef4e3aef817ca04f6c9b6151bd637df4c505
KolyaKhmarjuk <kolyakhmarjuk@gmail.com>
2022-12-13 15:11:06 +0200

    home work wrote

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,49 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
 
+const refs = {
+    onStartBtn: document.querySelector('button[data-start]'),
+    onStopBtn: document.querySelector('button[data-stop]')
+}
+
+let bgColor = null;
+let isActiv = null;
+
+refs.onStartBtn.addEventListener('click', () => {
+    if(isActiv){
+        return;
+    }
+    isActiv = true;
+    bgColor = setInterval(() => {
+    refs.onStartBtn.parentElement.style.backgroundColor = `${getRandomHexColor()}`;
+    }, 1000)
+})
+
+refs.onStopBtn.addEventListener('click', () => {
+    clearInterval(bgColor);
+    isActiv = false;
+})
+
+
+// const randomColor = {
+//     bgColor: null,
+//     isActiv: false,
+
+//     start(){
+//         if(this.isActiv){
+//             return;
+//         }
+//         this.isActiv = true;
+//         this.bgColor = setInterval(() => {
+//             refs.onStartBtn.parentElement.style.backgroundColor = `${getRandomHexColor()}`;
+//         }, 1000)
+//     },
+//     stop(){
+//         clearInterval(this.bgColor);
+//         this.isActiv = false;
+//     },
+// }
+
+// refs.onStartBtn.addEventListener('click', () => {randomColor.start()});
+// refs.onStopBtn.addEventListener('click', () => {randomColor.stop()});
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,36 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const refs = {
+  form: document.querySelector('.form'),
+  delayInput: document.querySelector('input[name="delay"]'),
+  stepInput: document.querySelector('input[name="step"]'),
+  amountInput: document.querySelector('input[name="amount"]')
+}
+
+refs.form.addEventListener('submit', iterationCreatePromise);
+
+function iterationCreatePromise(event) {
+  event.preventDefault();
+  delayInputValue = Number(refs.delayInput.value);
+  stepInputValue = Number(refs.stepInput.value);
+  createPositionNumber = 0;
+
+  for (let i = 0; i < refs.amountInput.value; i += 1){
+    createPromise(createPositionNumber += 1, delayInputValue + stepInputValue * i)
+    .then(result => console.log(result))
+    .catch(rej => console.log(rej));
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, rejected) => {
+  const shouldResolve = Math.random() > 0.3;
+  setTimeout(() => {
+    if(shouldResolve){
+      resolve(Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`));
+    }else{
+      rejected(Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`));
+    }
+  }, delay)
+})
 }
